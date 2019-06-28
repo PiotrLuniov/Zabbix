a0a0509460b436be77e40a4ae596758a7941c0f9
Piotr_Luniov <piotr_luniov@epam.com>
2019-06-28 20:36:24 +0300

    Zabix task01 ver.02

--- 01/zabbix01.sh
+++ 01/zabbix01.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+ZABBIX_CONF="/etc/zabbix/zabbix_server.conf"
+
 yum -y install vim mc net-tools
 yum -y install mariadb mariadb-server
 /usr/bin/mysql_install_db --user=mysql
@@ -11,26 +14,24 @@ EOM
 
 yum -y install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
 yum -y install zabbix-server-mysql zabbix-web-mysql
-#!!!test
+
 zcat /usr/share/doc/zabbix-server-mysql-*/create.sql.gz | mysql -uzabbix -pzabbix zabbix
-#<<EOM
-#password
-#EOM
 
-if [ $(grep -c "^DBHost=" "/etc/zabbix/zabbix_server.conf") -eq 0 ]; then
-  echo "DBHost=localhost" >> /etc/zabbix/zabbix_server.conf
+
+if [ $(grep -c "^DBHost=" $ZABBIX_CONF) -eq 0 ]; then
+  echo "DBHost=localhost" >> $ZABBIX_CONF
 fi
 
-if [ $(grep -c "^DBName=" "/etc/zabbix/zabbix_server.conf") -eq 0 ]; then
-  echo "DBName=zabbix" >> /etc/zabbix/zabbix_server.conf
+if [ $(grep -c "^DBName=" $ZABBIX_CONF) -eq 0 ]; then
+  echo "DBName=zabbix" >> $ZABBIX_CONF
 fi
 
-if [ $(grep -c "^DBUser=" "/etc/zabbix/zabbix_server.conf") -eq 0 ]; then
-  echo "DBUser=zabbix" >> /etc/zabbix/zabbix_server.conf
+if [ $(grep -c "^DBUser=" $ZABBIX_CONF) -eq 0 ]; then
+  echo "DBUser=zabbix" >> $ZABBIX_CONF
 fi
 
-if [ $(grep -c "^DBPassword=" "/etc/zabbix/zabbix_server.conf") -eq 0 ]; then
-  echo "DBPassword=zabbix" >> /etc/zabbix/zabbix_server.conf
+if [ $(grep -c "^DBPassword=" $ZABBIX_CONF) -eq 0 ]; then
+  echo "DBPassword=zabbix" >> $ZABBIX_CONF
 fi
 
 systemctl start zabbix-server
@@ -38,12 +39,3 @@ systemctl start zabbix-server
 sed -i '/\<IfModule mod_php5.c\>/a php_value date.timezone Europe\/Minsk' /etc/httpd/conf.d/zabbix.conf
 
 systemctl start httpd
-
-#Install agent
-#yum install zabbix-agent
-
-#sed -i 's/^Server=127\.0\.0\.1/Server=172\.33\.33\.22/' /etc/zabbix/zabbix_agentd.conf
-#sed -i 's/# ListenPort=10050/ListenPort=10050/' /etc/zabbix/zabbix_agentd.conf
-#sed -i 's/^ServerActive=127\.0\.0\.1/ServerActive=172\.33\.33\.22/' /etc/zabbix/zabbix_agentd.conf
-
-#systemctl start zabbix-agent
--- 01/zabbix02.sh
+++ 01/zabbix02.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+NGINX_CONF="/etc/nginx/nginx.conf"
+ZABBIXAGENT_CONF="/etc/zabbix/zabbix_agentd.conf"
+
 yum -y install vim mc net-tools
 yum -y install nginx java tomcat tomcat-webapps tomcat-admin-webapps
-sed -i 's-.*:80 .*--' /etc/nginx/nginx.conf
+
+sed -i '/#/d' $NGINX_CONF
+sed -i '/\[\:\:\]\:80/d' $NGINX_CONF
+sed -i '/location \/ /a proxy_pass http\:\/\/127\.0\.0\.1\:8080\/sample\/\;' $NGINX_CONF
+
 #Start service
+
 systemctl enable tomcat
 systemctl start tomcat
 systemctl enable nginx
@@ -13,9 +21,10 @@ yum -y install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.
 
 yum -y install zabbix-agent
 
-sed -i 's/^Server=127\.0\.0\.1/Server=172\.33\.33\.11/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/# ListenPort=10050/ListenPort=10050/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/^ServerActive=127\.0\.0\.1/ServerActive=172\.33\.33\.11/' /etc/zabbix/zabbix_agentd.conf
+sed -i '/#/d'
+sed -i 's/^Server=127\.0\.0\.1/Server=172\.33\.33\.11/' $ZABBIXAGENT_CONF
+sed -i 's/# ListenPort=10050/ListenPort=10050/' $ZABBIXAGENT_CONF
+sed -i 's/^ServerActive=127\.0\.0\.1/ServerActive=172\.33\.33\.11/' $ZABBIXAGENT_CONF
 
 systemctl start zabbix-agent
 
